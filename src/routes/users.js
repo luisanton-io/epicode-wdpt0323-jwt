f7898981b8dd84bde06f30878476500cd33df26a
luisanton-io <hello@luisanton.io>
2023-12-11 21:57:49 +0100

    Commenti

--- src/routes/users.js
+++ src/routes/users.js
@@ -58,8 +58,11 @@ usersRouter.post("/session", async (req, res) => {
 usersRouter.get(
     "/oauth-google",
     passport.authenticate("google", {
-        scope: ["profile", "email"],
-        prompt: "select_account",
+        scope: ["profile", "email"], // ovvero i dati che vogliamo ricevere da Google
+        prompt: "select_account", // questo per richiede all'utente di scegliere il proprio account,,
+        // altrimenti Google sceglie l'ultimo automaticamente,
+        // il che non sempre è desiderabile, ad esempio volendosi autenticare con un account diverso,
+        // volendo creare un nuovo account, o volendo sloggarsi
     })
 )
 
